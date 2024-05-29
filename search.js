352f885bde8340254500e960f0730c6ef39c5884
jahed088 <jahed2@hotmail.co.uk>
2024-05-30 00:17:27 +0100

    new3

--- search.js
+++ search.js
@@ -4,11 +4,12 @@ var apiKey = 'da97fcc72d5377406b5da395ade6f5f5';
 var index = 'JSON';
 
 var client = algoliasearch(applicationID, apiKey);
-var helper = algoliasearchHelper(client, index);
-
-//var algolia = algoliasearch('TS106XF7Y9', 'da97fcc72d5377406b5da395ade6f5f5');
+var helper = algoliasearchHelper(client, index, {
+  facets: ['food_type'] 
+});
 
 helper.on('result', function(content) {
+  renderFacetList(content); // not implemented yet
   renderHits(content);
 });
 
@@ -16,6 +17,26 @@ function renderHits(content) {
   $('#container').html(function() {
     return $.map(content.hits, function(hit) {
       return '<li>' + hit._highlightResult.name.value + '</li>';
+    });
+  });
+}
+
+$('#facet-list').on('click', 'input[type=checkbox]', function(e) {
+  var facetValue = $(this).data('facet');  
+  helper.toggleRefinement('food_type', facetValue)
+        .search();
+});
+
+function renderFacetList(content) {
+  $('#facet-list').html(function() {
+    return $.map(content.getFacetValues('food_type'), function(facet) {
+      var checkbox = $('<input type=checkbox>')
+        .data('facet', facet.name)
+        .attr('id', 'fl-' + facet.name);
+      if(facet.isRefined) checkbox.attr('checked', 'checked');
+      var label = $('<label>').html(facet.name + ' (' + facet.count + ')')
+                              .attr('for', 'fl-' + facet.name);
+      return $('<li>').append(checkbox).append(label);
     });
   });
 }
